b35b2587c3e09d94075895185c0b708e1c01d0c7
JulioUSCS <julio.bernardino@uscsonline.com.br>
2025-05-28 08:31:04 -0300

    Update historicoController.js

--- controller/historicoController.js
+++ controller/historicoController.js
@@ -11,25 +11,7 @@ const historicoController = {
             res.status(500).json({ sucesso: false, mensagem: 'Erro interno do servidor ao buscar histórico.' });
         }
     },
-
-    // Você pode remover este método se não for usá-lo por agora,
-    // ou mantê-lo para futura expansão, mas ele requer uma SP com parâmetro.
-    // async getHistoricoPorId(req, res) {
-    //     const idAtendimento = req.params.id;
-    //     if (!idAtendimento) {
-    //         return res.status(400).json({ sucesso: false, mensagem: 'ID do atendimento é obrigatório.' });
-    //     }
-    //     try {
-    //         const historico = await Historico.listarHistoricoPorIdAtendimento(idAtendimento);
-    //         if (historico.length === 0) {
-    //             return res.status(404).json({ sucesso: false, mensagem: 'Histórico não encontrado para o ID fornecido.' });
-    //         }
-    //         res.json({ sucesso: true, historico: historico });
-    //     } catch (error) {
-    //         console.error('Erro no controller ao buscar histórico por ID:', error.message);
-    //         res.status(500).json({ sucesso: false, mensagem: 'Erro interno do servidor ao buscar histórico por ID.' });
-    //     }
-    // }
+    
 };
 
 module.exports = historicoController;
